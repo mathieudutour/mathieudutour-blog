e767a1862a5f2198228c0bff871a330fa493b067
mathieudutour <mathieu@dutour.me>
2019-08-01 10:00:54 +0200

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Mathieu Dutour',
   githubUrl: 'https://github.com/mathieudutour/mathieudutour-blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://mathieudutour-blog.netlify.com',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'MathieuDutour',
